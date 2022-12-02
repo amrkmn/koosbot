b2177aaaff54db82f8485f9e229ec8e625c2bb24
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-12-02 23:41:33 +0800

    Update main.ts

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -4,7 +4,7 @@ import { ApiRequest, ApiResponse, methods, Route } from "@sapphire/plugin-api";
 @ApplyOptions<Route.Options>({ route: `` })
 export class UserRoute extends Route {
     public [methods.GET](_request: ApiRequest, response: ApiResponse) {
-        response.json({ message: "Landing Page!" });
+        response.json({ message: `Landing page for ${this.container.client.user?.tag}` });
     }
 
     public [methods.POST](_request: ApiRequest, response: ApiResponse) {
