2815e611a2384cdce3b26b418368f69f5c5e76af
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-12-03 00:27:09 +0800

    Update playerEmpty.ts

--- src/listeners/kazagumo/playerEmpty.ts
+++ src/listeners/kazagumo/playerEmpty.ts
@@ -1,3 +1,4 @@
+import { embedColor } from "#utils/constants";
 import { ApplyOptions } from "@sapphire/decorators";
 import { container, Listener } from "@sapphire/framework";
 import { Events, KazagumoPlayer } from "kazagumo";
@@ -9,6 +10,9 @@ import { Events, KazagumoPlayer } from "kazagumo";
 })
 export class ClientListener extends Listener {
     public async run(player: KazagumoPlayer) {
-        this.container.logger.debug("playerEmpty", player);
+        const channel = container.client.channels.cache.get(player.textId) ?? (await container.client.channels.fetch(player.textId));
+        if (!channel) return;
+
+        if (channel.isText()) channel.send({ embeds: [{ description: "There are no more tracks", color: embedColor.red }] });
     }
 }
