3fe1ccc32a08a23362bdf6e7184439b7192186a4
aytea14 <muhdamar4687@gmail.com>
2022-12-05 23:51:17 +0800

    Update main.ts

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,13 +1,19 @@
 import { ApplyOptions } from "@sapphire/decorators";
+import { DurationFormatter } from "@sapphire/duration";
 import { ApiRequest, ApiResponse, methods, Route } from "@sapphire/plugin-api";
 
+const formatter = new DurationFormatter();
 @ApplyOptions<Route.Options>({ route: `` })
 export class UserRoute extends Route {
     public [methods.GET](_request: ApiRequest, response: ApiResponse) {
-        response.json({ message: `Landing page for ${this.container.client.user?.tag}` });
+        const uptime = formatter.format(this.container.client.uptime!);
+
+        response.json({ message: `Landing page for ${this.container.client.user?.tag}`, uptime });
     }
 
     public [methods.POST](_request: ApiRequest, response: ApiResponse) {
-        response.json({ message: `Landing page for ${this.container.client.user?.tag}` });
+        const uptime = formatter.format(this.container.client.uptime!);
+
+        response.json({ message: `Landing page for ${this.container.client.user?.tag}`, uptime });
     }
 }
