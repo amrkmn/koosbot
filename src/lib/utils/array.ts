ca86074de4f787ce34485fcb51fb638686bde590
aytea14 <85985879+AyTea14@users.noreply.github.com>
2023-06-01 16:13:16 +0800

    Update array.ts

--- src/lib/utils/array.ts
+++ src/lib/utils/array.ts
@@ -4,6 +4,7 @@ export function first<T>(array: T[]): T | undefined;
 export function first<T>(array: T[], amount: number): T[];
 export function first<T>(array: T[], amount?: number): T | T[] | undefined {
     const newArray = [...array];
+
     if (amount === undefined) return newArray.at(0);
     if (amount < 0) return last(newArray, amount * -1);
     amount = Math.min(newArray.length, amount);
@@ -14,6 +15,7 @@ export function last<T>(array: T[]): T | undefined;
 export function last<T>(array: T[], amount: number): T[];
 export function last<T>(array: T[], amount?: number): T | T[] | undefined {
     const newArray = [...array];
+
     if (amount === undefined) return newArray.at(-1);
     if (amount < 0) return first(newArray, amount * -1);
     if (!amount) return [];
@@ -24,10 +26,28 @@ export function random<T>(array: T[]): T | undefined;
 export function random<T>(array: T[], amount: number): T[];
 export function random<T>(array: T[], amount?: number): T | T[] | undefined {
     const newArray = [...array];
+
     if (amount === undefined) return newArray[randomInt(newArray.length)];
     if (!newArray.length || !amount) return [];
     return Array.from(
         { length: Math.min(amount, newArray.length) }, //
         () => newArray.splice(randomInt(newArray.length), 1)[0]!
     );
+}
+
+export function remove<T>(array: T[], position: number): T[];
+export function remove<T>(array: T[], position: number, to: number): T[];
+export function remove<T>(array: T[], position: number, to?: number): T[] {
+    const newArray = [...array];
+
+    if (position === to) to = undefined;
+    if (to && to < position) to = undefined;
+
+    if (position < 0) throw new Error(`The "position" value must be from 0 to ${newArray.length - 1}`);
+    if (position > newArray.length - 1 || (to && to > newArray.length - 1))
+        throw new Error(`The "position" or "to" value greater than ${newArray.length - 1}`);
+
+    const count = to ? to - position + 1 : 1;
+    newArray.splice(position, count);
+    return newArray;
 }
