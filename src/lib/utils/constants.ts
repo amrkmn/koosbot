62c9142a81c6b4ecf040155438f080f64664e7d4
amrkmn <muhdamar4687@gmail.com>
2024-08-25 03:08:54 +0800

    fix: use correct auth for lavalink instance

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -7,7 +7,7 @@ export const UserAgent =
 export const Nodes: NodeOption[] = [
     { name: "node1", url: "v4.lavalink.rocks:443", auth: "horizxon.tech", secure: true },
     { name: "node2", url: "v4.lavalink.rocks:80", auth: "horizxon.tech", secure: false },
-    { name: "node3", url: "lavalink.ujol.dev:443", auth: "youshallnotpass", secure: true },
+    { name: "node3", url: "lavalink.ujol.dev:443", auth: "pleasegoaway", secure: true },
 ];
 
 export const Regex = {
