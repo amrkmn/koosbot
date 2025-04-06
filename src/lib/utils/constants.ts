0dfe792d57a6238657503847a9116cddae3e3df9
amrkmn <amartukiminj@gmail.com>
2025-04-06 11:01:19 +0800

    Update constants.ts

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -6,7 +6,7 @@ export const zws = "\u200B";
 export const UserAgent =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36";
 
-export const Nodes: NodeOption[] = envParseArray("LAVALINK_NODES", ["wss://pleasegoaway@lavalink.ujol.dev:443"]).map(parseNodeOption);
+export const Nodes: NodeOption[] = envParseArray("LAVALINK_NODES", []).map(parseNodeOption);
 
 export const Regex = {
     Youtube: /(youtu\.be\/|youtube\.com\/)/g,
